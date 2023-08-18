a6d5fc6d2d3768f1f22cb5e448bad4b91a3bd2b6
Pisteur_alpin <pisteur.alpin@gmail.com>
2023-08-18 14:53:29 +0200

    ♻️ Restart - Migrate to Django Rest Framework

--- app/api/api_v1/api.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter
-
-from app.api.api_v1.endpoints import template, profile
-
-api_router = APIRouter()
-api_router.include_router(template.router, prefix="/template", tags=["temp"])
-api_router.include_router(profile.router, prefix="/profile", tags=["profile"])
---
+++ respublica/respublicaapi/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class RespublicaapiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'respublicaapi'
